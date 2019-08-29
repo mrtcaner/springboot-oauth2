9699aa39291ea8837fd24248fc483b675f547f7e
mrtcaner <muratcaner@ymail.com>
2019-08-29 23:46:33 +0300

    clean up

--- oauth2-client/src/main/java/com/my/edu/oauth2/client/oauth2client/data/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my.edu.oauth2.client.oauth2client.data.repository;
-
-import com.my.edu.oauth2.client.oauth2client.data.model.Event;
-import com.my.edu.oauth2.client.oauth2client.data.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
